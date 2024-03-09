2e5e864895d356688ce1176c49ceaf526dfcd0f9
hannansohail4u <154316851+hannansohail4u@users.noreply.github.com>
2024-03-09 12:11:13 +0500

    Add files via upload

---
+++ Assigment 1/app.js
@@ -0,0 +1,78 @@
+// Chapter 1
+
+// alert("Error! plz enter a valid password")
+
+// alert("Welcome to JS land...\nHappy Coding!")
+
+
+// alert("Welcome to JS land")
+// alert("Happy Coding!\nPrevent this page from creating additional dialogues.")
+
+// Chapter 2
+
+// var username = "jhone doe";
+// document.write(username)
+
+// var myname = "Certified Mobile Application Development";
+// document.write(myname)
+
+// var age = "15 years old";
+
+// var message = "HELLO WORLD"
+// alert(message)
+
+// alert(username)
+// alert(age,"years old")
+// alert(myname)
+
+// Chapter 3
+
+// var age = 15;
+// alert("I am " + age + " years old")
+
+// var visitor = 14;
+// alert("you have visited this site " + visitor + " times.")
+
+// var birthyear = 1990;
+// document.write( " My birth year is " + birthyear)
+
+// Chapter 4
+
+// var [x,y,z] = [1,2,3]
+// alert(x)
+
+// illegal
+// var var = "10"
+// var alert = "john"
+
+// legal
+// var $10 = "Hannan"
+// alert($10)
+
+// Chapter 5
+
+// var num1 = 5;
+// var num2 = 3;
+// var add = 5 + 3;
+// document.write(add)
+
+// var num1 = 5;
+// var num1 = num1 + 1;
+// console.log(num1)
+
+// var num1 = 5;
+// var num1 = num1 - 1;
+// console.log(num1)
+
+// Chapter 6
+
+// var a = 10;
+// a = ++a;
+// console.log(a)
+
+// a = --a;
+// console.log(a)
+
+// a = a--;
+// console.log(a)
+
